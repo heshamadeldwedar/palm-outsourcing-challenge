89d345d7ad05b23af28e3c6959dd150ce70003a5
Hesham Adel <hesham.adel@pickspace.com>
2025-01-06 04:54:05 +0200

    adds tasks table migrations
    adds tasks table migrations

--- backend/app/Http/Controllers/Task/TaskController.php
+++ backend/app/Http/Controllers/Task/TaskController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class TaskController extends Controller
 {
     function index() {
@@ -11,8 +9,21 @@ class TaskController extends Controller
             [
                 'id' => 1,
                 'title' => 'this is a title',
-                'description' => 'this is a description'
+                'description' => 'this is a description',
+                'status' => 'in-progress'
             ],
+            [
+                'id' => 2,
+                'title' => 'this is yet another title',
+                'description' => 'this is yet another description',
+                'status' => 'backlog'
+            ],
+            [
+                'id' => 3,
+                'title' => 'this is yest another title',
+                'description' => 'this is yet the last description',
+                'status' => 'live-approved'
+            ]
         ];
     }
 }
